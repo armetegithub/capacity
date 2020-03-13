1c749a2925a6d71372428a2557e074aad7ef4bd2
armetegithub <armomomete@protonmail.com>
2020-03-13 10:53:09 +0100

    María 4

--- server/models/Project.js
+++ server/models/Project.js
@@ -5,7 +5,7 @@ const projectSchema = new Schema({
   name: {type: String, required: true },
   info:{type: String, required: true},
   imageURL: String,
-  project_type: {type: String, enum:['actividad', 'trabajo', 'beca']},
+  project_type: {type: String, enum:['ACTIVIDAD', 'TRABAJO', 'BECA']},
   foundation: {
       type: Schema.Types.ObjectId,
       ref: "Foundation"
